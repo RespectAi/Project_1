94472552dc576f89c575ed8718fdbfdd81c97b39
RespectedAi <mabiodun611@gmail.com>
2025-03-12 02:53:24 +0100

    Favorite Colour

--- Python Lessons/Lesson1.py
+++ Python Lessons/Lesson1.py
@@ -1,1 +1,19 @@
-print("Hello wrold")
+print("Hello world")
+
+# list in python 
+
+languages = ["HTML","j script","0909","5.002"]
+print(languages[0])
+print(languages[-2])
+
+# personalized greeting app 
+
+print("Welcome to CLS APP")
+name = input("Please provide your name: ")
+color = input("Please provide your favorite color: ")
+colors = ["red", "blue", "green", "yellow", "black", "white", "purple", "pink", "orange", "brown"]
+import random
+favorite_color = random.choice(colors)
+import emoji 
+print(f"Hello {name}!, Welcome to CLS app. Your favorite color is {color}....But the favorite color assign to you is {favorite_color}\U0001F608")
+
