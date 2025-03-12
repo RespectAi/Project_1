0a3e7424eb03569ef5039c60d31a3b4fc7be2163
RespectedAi <mabiodun611@gmail.com>
2025-03-12 04:21:36 +0100

    Second Commit

---
+++ Python Lessons/Lesson1.2.py
@@ -0,0 +1,19 @@
+import random
+
+# List of programming jokes
+jokes = [
+    "Why do Python programmers prefer dark mode? Because light attracts bugs! 🐛😂",
+    "Why do programmers hate nature? Too many bugs. 🪲",
+    "Why was the JavaScript developer sad? Because he didn't 'null' his problems. 😢",
+    "How do you comfort a JavaScript bug? You console it. 🖥️",
+    "Why did the Python function break up with the loop? It needed some space (indentation issues)! 🐍",
+    "Why was the developer unhappy at their job? They wanted arrays, but all they got were lists. 😆",
+    "Why did the developer go broke? Because they used up all their cache! 💰",
+    "What's a programmer's favorite hangout place? The Foo Bar! 🍻",
+    "Why do Java programmers wear glasses? Because they don't see sharp! 👓",
+    "How does a computer get drunk? It takes screenshots! 📸",
+]
+
+# Randomly select and display a joke
+print("\n🤣 Here's a programming joke for you:")
+print(random.choice(jokes))
